a34ecc47627ee4b339616aa0df94d14a99eca2f6
Benjie Malinao <benjiemalinao87@gmail.com>
2018-04-29 13:46:29 +1000

    Added task item code block to ShopingApp.py

--- ShoppingApp.py
+++ ShoppingApp.py
@@ -2,9 +2,21 @@
 
 # Welcome Message
 # Menu - Choices
-<<<<<<<
-# Get User Input 
-=======
+# Get User Input
 # Get User Input (Validate User Input - Except & try)
+# Get User-Input Category
+# Get Customer Purchase
+# Write Customer Purchased to a database file
+# Add Customer name to a Database File
+# Get Customer Reward Card ID
+# Validate Customer Reward Card ID (Exist not Exist)
+# Compute Customer Point Reward (Every 100 == 1 Pts)
+# Add Process here
+# _______________
+# _______________
+# _______________
+# _______________
+# _______________
 
->>>>>>>
+# End - Print Purchased Item
+# End - Print Points Reward
