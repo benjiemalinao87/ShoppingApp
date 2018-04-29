f24c188fd8d77ab90f99ca8cec9fa9577f7b807f
Mark Kelvin Libario <mkelvin.libario@gmail.com>
2018-04-29 01:54:40 -0400

    added sample menu

--- ShoppingApp.py
+++ ShoppingApp.py
@@ -1,8 +1,22 @@
 # @@@@@@@@@@@@  SHOPPING APP @@@@@@@@@@@@
 
-# Welcome Message
 # Menu - Choices
+
+food = ['Spaghetti', 'Burger', 'Chicken', 'Hotdog']
+beverages = ['Waterbottle', 'Softdrinks', 'Juice', 'Smoothies']
+
+menus = {'food': {'spaghetti': 3.25, 'burger': 4.75, 'chicken': 3.5, 'hotdog': 1.25},
+         'beverages': {'waterbottle': 0.99, 'softdrinks': 1.5, 'juice': 1.25, 'smoothies': 2.25}}
+# Welcome Message
+print('Welcome to ABC\'s Mini Cafeteria!')
+print('Today\'s current food menu is:\n{}.\n'.format(', '.join(food)))
+print('The available beverages are:\n{}'.format(','.join(beverages)))
+
+
 # Get User Input
+print('Please input your food choice from the list above:')
+user_food = input('> ').lower()
+
 # Get User Input (Validate User Input - Except & try)
 # Get User-Input Category
 # Get Customer Purchase
@@ -20,3 +34,4 @@
 
 # End - Print Purchased Item
 # End - Print Points Reward
+
